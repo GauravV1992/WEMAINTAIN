5579197a0c51e54c0b671643304fdfc674e39eb9
gaurav sharma <gaurav.netdev@Gmail.com>
2023-05-28 00:29:51 +0530

    merged

--- BusinessEntities/RequestDto/CategoryRequest.cs
+++ BusinessEntities/RequestDto/CategoryRequest.cs
@@ -8,6 +8,8 @@ namespace BusinessEntities.RequestDto
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+
+        public string?  Description { get; set; }
         public string? Ext { get; set; }
 
     }
